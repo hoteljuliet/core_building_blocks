d6be94abc4987c0f70a9461934b1c2e5cdd795d1
Colasurdo, Dennis <Dennis_Colasurdo@cable.comcast.com>
2017-11-11 10:02:09 -0500

    Initial Checkin

---
+++ src/test/java/org/dac/core_building_blocks/CoreBuildingBlocksApplicationTests.java
@@ -0,0 +1,16 @@
+package org.dac.core_building_blocks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoreBuildingBlocksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
